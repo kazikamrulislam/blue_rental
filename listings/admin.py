e480a01e9d5d22373aa14697da1baa26ccbb086f
kazikamrulislam <kamrul.lab@gmail.com>
2024-03-23 00:00:06 +0600

    listing model updated

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing, Realtor, Amenity, Rules_Terms, Address
+admin.site.register(Realtor)
+admin.site.register(Amenity)
+admin.site.register(Rules_Terms)
+admin.site.register(Address)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_available', 'list_date', 'is_published')
+    list_display_links = ('id', 'title')
+    list_filter = ('amenities',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'address', 'zipcode', 'description', 'bedrooms', 'sqft', 'state')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
