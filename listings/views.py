019d413c6bdabbd4d49298511e211d93684d9b60
kazikamrulislam <kamrul.lab@gmail.com>
2024-03-29 00:13:38 +0600

    listing view updated with pagination

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import get_object_or_404, render
+# from listings.choices import bedroom_choices, price_choices, state_choices 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing 
 
 def  index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 8)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
